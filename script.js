2c409ab001125f09c41c02762aeddf8c3889ab29
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-07-22 16:56:20 -0400

    [UPDATE]--- Added new color scheme to the cal layout. Not finalized. Will start working on JS w/ buttons.

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+/*
+For the calculator to work.. 
+I need each button to capture user input & save it 
+ie: any numeral value & the function has to continue running 
+until an arthimetric value is pressed. 
+(& the decimal point has to be pressed ONCE and then break (maybe a switch statement?)
+then; after an arthmetric value is pressed, the user can enter another number
+then the calculation is made with enter.
+
+So: 2 values have to be saved. num1 & num2
 
 
-function ('Calculate') {
+
+*/
+function Calculate ('Calculate') {
 
 }
