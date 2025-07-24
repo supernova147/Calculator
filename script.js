e2d9d87f6c370bfdf34d56e9845b3fe5c956cfad
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-07-24 09:23:36 -0400

    [UPDATE]---Basic functionality added to the calculator; Further updates will include updates to the CSS & additional functionality.

--- script.js
+++ script.js
@@ -9,9 +9,205 @@ then the calculation is made with enter.
 
 So: 2 values have to be saved. num1 & num2
 
-
-
 */
-function Calculate ('Calculate') {
+let output = document.getElementById('displayed_output');
 
+
+function pressed_1() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementsByClassName('cal_buttons');
+        output.innerText = '1';
+    }
+    else 
+    {
+        output.innerText += '1';
+    }
 }
+
+function pressed_2() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.append = '2';
+    }
+    else 
+    {
+        output.innerText += '2';
+    }
+}
+
+function pressed_3() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '3';
+    }
+    else 
+    {
+        output.innerText += '3';
+    }
+}
+
+function pressed_4() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '4';
+    }
+    else 
+    {
+        output.innerText += '4';
+    }
+}
+
+function pressed_5() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '5';
+    }
+    else 
+    {
+        output.innerText += '5';
+    }
+}
+
+function pressed_6() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '6';
+    }
+    else 
+    {
+        output.innerText += '6';
+    }
+}
+
+function pressed_7() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '7';
+    }
+    else 
+    {
+        output.innerText += '7';
+    }
+}
+
+function pressed_8() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '8';
+    }
+    else 
+    {
+        output.innerText += '8';
+    }
+}
+
+function pressed_9() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '9';
+    }
+    else 
+    {
+        output.innerText += '9';
+    }
+}
+
+function pressed_divide() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '/';
+    }
+    else 
+    {
+        output.innerText += '/';
+    }
+}
+
+function pressed_multiple() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '*';
+    }
+    else 
+    {
+        output.innerText += '*';
+    }
+}
+
+function pressed_subtract() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '-';
+    }
+    else 
+    {
+        output.innerText += '-';
+    }
+}
+
+function pressed_addition() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '+';
+    }
+    else 
+    {
+        output.innerText += '+';
+    }
+}
+
+function pressed_percentage() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '%';
+    }
+    else 
+    {
+        output.innerText += '%';
+    }
+}
+
+function pressed_clear() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText = '';
+    }
+    else 
+    {
+        output.innerText = '';
+    }
+}
+
+function calculate() {
+    if (displayed_output == 'Enter a calculation!')
+    {
+        let output = document.getElementById('displayed_output');
+        output.innerText += '';
+    }
+    else 
+    {
+        output.innerHTML = eval(output.innerText);
+    }
+}
+
+//dyanmic array for memory? and then a function to clear the memory.
+// Make the input function a loop
+//percentage button?
+//color button function?
+//special button - maybe an animation?
+//afterwards, may provide additional calc functions ie: exponentials, etc.
